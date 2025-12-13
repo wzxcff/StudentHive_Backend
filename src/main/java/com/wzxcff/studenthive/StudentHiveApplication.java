68063c6d5b22e774208e72b9edc826de68d51d91
wzxcff <Shevasfloyd@gmail.com>
2025-12-13 13:56:33 +0100

    First commit

---
+++ src/main/java/com/wzxcff/studenthive/StudentHiveApplication.java
@@ -0,0 +1,13 @@
+package com.wzxcff.studenthive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentHiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzxcff/studenthive/StudentHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzxcff.studenthive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
