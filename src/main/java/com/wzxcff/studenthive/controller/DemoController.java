dfa2917617102eb679e746eae3ce0a1825f7f411
wzxcff <Shevasfloyd@gmail.com>
2025-12-14 13:07:53 +0100

    DemoController for testing added

---
+++ src/main/java/com/wzxcff/studenthive/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.wzxcff.studenthive.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> demo() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
